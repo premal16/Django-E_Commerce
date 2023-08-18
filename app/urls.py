06a005c1b72f3a26133f4f48eb72172497f4953a
premal <premal@moontechnolabs.com>
2023-08-18 13:44:40 +0530

    update the profile view for both logged user or admin user

--- app/urls.py
+++ app/urls.py
@@ -4,11 +4,15 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.homePage, name='home'),
     path('login/', views.login, name='login'),
     path('register/', views.register_and_login, name='register'),
     path('profile/', views.profilePage, name='profile'),
     path('logout/', views.logout_view, name='logout'),
+    # path('edit-profile/', views.edit_profile, name='edit_profile'),
+    # path('edit-profile/<int:user_id>/', views.edit_profile, name='admin_edit_profile'),
     path('edit-profile/', views.edit_profile, name='edit_profile'),
+    path('edit-profile/<int:user_id>/', views.edit_profile, name='edit_profile_with_id'),
     path('contact/', views.contactPage, name='contact'),
     path('change-password/', views.change_password, name='change_password'),
     path('products/<int:product_id>/', views.product_detail, name='product-detail'),
@@ -17,9 +21,7 @@ urlpatterns = [
     path('orders/', views.OrderListView.as_view(), name='admin-order-list'),
     path('orders/<int:pk>/', views.OrderDetailView.as_view(), name='admin-order-detail'),
     path('order/<int:order_id>/change-status/', views.change_order_status, name='admin-order-change-status'),
-    path('',views.homePage, name='home'),
     path('users/',views.user, name='user'),
     path('users/<int:pk>/',views.UserDetailview.as_view(), name='user-profile'),
-
-
+    path('users/delete/<int:user_id>/', views.custom_user_delete, name='custom-user-delete'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponse
 from django.contrib.auth import get_user_model
 from .models import CustomUser,UserProfile,Product,Order
 from django.contrib.auth import authenticate
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth import logout
 from django.db import IntegrityError
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required,user_passes_test
 from django.shortcuts import render, get_object_or_404
 from .forms import ProductForm
 from django.views.generic import ListView, DetailView
@@ -72,43 +72,106 @@ def logout_view(request):
 
 
 
-@login_required(login_url='/login/')
-def edit_profile(request):
-    try:
-        user = request.user
-        print(user.email)
-        profile, created = UserProfile.objects.get_or_create(user=user)
-        error_message = None 
-        if request.method == 'POST':
-            try:
-                about = request.POST.get('about', '')
-                profile_pic = request.FILES.get('profile_pic', None)
-                job_title = request.POST.get('job_title', '')
-                country = request.POST.get('country', '')
-                address = request.POST.get('address', '')
-                email = request.POST.get('email', '')
-                mobile_number = request.POST.get('mobile_number')
-                profile.job_title = job_title
-                # Update profile fields and save
-                profile.about = about
-                profile.country = country
-                profile.address = address
-                profile.mobile_number = mobile_number
-                user.email = email
-                user.save()
-                if profile_pic:
-                    profile.profile_pic = profile_pic
-                profile.save()
-                return redirect('profile')  
-            except IntegrityError as e:
-                if 'UNIQUE constraint' in str(e) and 'email' in str(e):
-                    error_message = "The provided email is already in use."
-                else:
-                    error_message = "An error occurred while updating the profile." 
+# @login_required(login_url='/login/')
+# def edit_profile(request):
+#     try:
+#         user = request.user 
+#         print(user.email)
+#         profile, created = UserProfile.objects.get_or_create(user=user)
+#         error_message = None 
+#         if request.method == 'POST':
+#             try:
+#                 about = request.POST.get('about', '')
+#                 profile_pic = request.FILES.get('profile_pic', None)
+#                 job_title = request.POST.get('job_title', '')
+#                 country = request.POST.get('country', '')
+#                 address = request.POST.get('address', '')
+#                 email = request.POST.get('email', '')
+#                 mobile_number = request.POST.get('mobile_number')
+#                 profile.job_title = job_title
+#                 # Update profile fields and save
+#                 profile.about = about
+#                 profile.country = country
+#                 profile.address = address
+#                 profile.mobile_number = mobile_number
+#                 user.email = email
+#                 user.save()
+#                 if profile_pic:
+#                     profile.profile_pic = profile_pic
+#                 profile.save()
+#                 return redirect('profile')  
+#             except IntegrityError as e:
+#                 if 'UNIQUE constraint' in str(e) and 'email' in str(e):
+#                     error_message = "The provided email is already in use."
+#                 else:
+#                     error_message = "An error occurred while updating the profile." 
                 
-        return render(request, 'profile.html', {'error_message': error_message})
-    except Exception as e:
-        print(e)
+#         return render(request, 'profile.html', {'error_message': error_message})
+#     except Exception as e:
+#         print(e)
+
+
+
+
+@login_required(login_url='/login/')
+def edit_profile(request, user_id=None):
+    error_message = None 
+    if user_id is None:
+        user = request.user
+        profile, created = UserProfile.objects.get_or_create(user=user)
+
+        print("this is logged user...............")
+    else:
+        if request.user.is_superuser:
+            user = CustomUser.objects.get(id=user_id)
+            profile = user.userprofile
+            print("this is by admin..............")
+        else:
+            return HttpResponse("You don't have permission to edit this user's profile.")
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    if request.method == 'POST':
+        try:
+            about = request.POST.get('about', '')
+            profile_pic = request.FILES.get('profile_pic', None)
+            job_title = request.POST.get('job_title', '')
+            country = request.POST.get('country', '')
+            address = request.POST.get('address', '')
+            email = request.POST.get('email', '')
+            mobile_number = request.POST.get('mobile_number')
+            profile.job_title = job_title
+            # Update profile fields and save
+            profile.about = about
+            profile.country = country
+            profile.address = address
+            profile.mobile_number = mobile_number
+            user.email = email
+            user.save()
+            if profile_pic:
+                profile.profile_pic = profile_pic
+            profile.save()
+            print(user_id)
+            if user_id is None:
+                print("innnnn")
+                return redirect('profile')
+            
+            else:
+                print('out.......')
+                return redirect('user-profile',pk=user.id)  
+
+        except IntegrityError as e:
+            if 'UNIQUE constraint' in str(e) and 'email' in str(e):
+                error_message = "The provided email is already in use."
+            else:
+                error_message = "An error occurred while updating the profile." 
+    # return render(request, 'profile.html', {'error_message': error_message})
+
+
+    return render(request, 'user_profile.html', {'error_message': error_message})
+
+
+
+
+
 
 @login_required(login_url='/login/')
 def change_password(request):
@@ -189,14 +252,27 @@ def user(request):
     context = {'products': products,'users':users,'orders':orders}
     return render(request,'user_list.html',context)
 
-# def userprofile(request,user_id):
-#     user = get_object_or_404(UserProfile,pk=user_id)
-#     context = {'user': user}
-#     return render(request, 'user_profile.html', context)
-
 
 class UserDetailview(DetailView):
     print("@@@@@@@@@@@@@")
     model = UserProfile
     template_name = 'user_profile.html'
     context_object_name = 'user'
+
+
+
+CustomUser = get_user_model()
+@login_required
+@user_passes_test(lambda user: user.is_superuser)
+def custom_user_delete(request, user_id):
+    user = CustomUser.objects.get(id=user_id)
+    print(user)
+    
+    if request.method == 'POST':
+        if user.is_superuser:
+            messages.error(request, "Cannot delete a superuser.")
+        else:
+            user.delete()
+            messages.success(request, f"User {user.username} has been deleted.")
+        return redirect('user') 
+    return render(request, 'user_list.html', {'user': user})
